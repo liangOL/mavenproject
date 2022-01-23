9c27fbede6b0bb287aad6fba4685508f13080ffb
赵亮呀 <1039511450@qq.com>
2022-01-24 07:26:38 +0800

    log4j添加

---
+++ mavenproject/src/main/java/controller/Mycontroller.java
@@ -0,0 +1,15 @@
+package controller;
+
+/**
+ * @ClassName Mycontroller
+ * @Desc
+ * @Author zhaoliang
+ * @Date 2022/1/22 19:08
+ * @Version 1.0
+ **/
+public class Mycontroller {
+
+    public String getIndex(){
+        return "";
+    }
+}
