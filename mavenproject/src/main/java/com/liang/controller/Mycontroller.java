7e02b776d205b35ae19fd7b31d3f2b083c48ad50
zhaoliang0828 <1039511450@qq.com>
2022-01-26 15:08:43 +0800

    3

--- mavenproject/src/main/java/com/liang/controller/Mycontroller.java
+++ mavenproject/src/main/java/com/liang/controller/Mycontroller.java
@@ -1,11 +1,13 @@
-package controller;
+package com.liang.controller;
 
-import org.apache.logging.log4j.core.Logger;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 
 
 /**
@@ -16,12 +18,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @Version 1.0
  **/
 @Controller
+@RequestMapping(value = "/my")
 public class Mycontroller {
-    Logger logger = (Logger) LoggerFactory.getLogger(Mycontroller.class);
-    @RequestMapping(value = "/getIndex")
+    private final static Logger logger = LoggerFactory.getLogger(Mycontroller.class);
+
+    @RequestMapping(value = "/getIndex", method = RequestMethod.POST)
     @ResponseBody
     public String getIndex(HttpServletRequest request){
-        logger.info("12345");
+        logger.info("开始");
         return "hello world!";
     }
 }
